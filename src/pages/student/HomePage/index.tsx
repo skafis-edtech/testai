d2857fdb9a08f0e67edfd49a91cf04b0139825ae
Naglis <naglis1134@gmail.com>
2024-05-13 19:38:16 +0300

    Homepage done

--- src/pages/student/HomePage/index.tsx
+++ src/pages/student/HomePage/index.tsx
@@ -1,7 +1,66 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./index.css";
+
 const HomePage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const regex = /^[a-zA-Z]*$/;
+  const [testCode, setTestCode] = useState<string>("");
+
+  const [studentId, setStudentId] = useState<string>("");
+
+  const gotoTest = (event: any) => {
+    event.preventDefault();
+    if (studentId === "" || testCode === "") {
+      alert("Prašome įvesti ID ir testo kodą");
+      return;
+    }
+    navigate(`/test/${testCode}/${studentId}`);
+  };
+
+  const gotoResults = (event: any) => {
+    event.preventDefault();
+    if (studentId === "" || testCode === "") {
+      alert("Prašome įvesti ID ir testo kodą");
+      return;
+    }
+    navigate(`/grade/${testCode}/${studentId}`);
+  };
+
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="page-container">
+      <div className="test-starter-container">
+        <h1>Testavimo aplinka Skafis</h1>
+        <h2>
+          DĖMESIO! Ši Skafis versija nėra pastovi! Nelaikykite platformoje
+          ilgalaikių duomenų.
+        </h2>
+        <p>Jūsų ID ir testo kodas skelbiami mokytojo</p>
+        <form onSubmit={gotoTest}>
+          <input
+            type="text"
+            placeholder="Įveskite savo ID"
+            value={studentId}
+            onChange={(e) => setStudentId(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Įveskite testo kodą"
+            value={testCode}
+            onChange={(e) => {
+              if (regex.test(e.target.value)) {
+                setTestCode(e.target.value.toUpperCase());
+              }
+            }}
+          />
+          <button type="submit">Pradėti testą</button>
+        </form>
+        <button onClick={gotoResults}>Pasitikrinti įvertinimą</button>
+      </div>
+      <button className="gotoDashboard" onClick={() => navigate("/dashboard")}>
+        Mokytojo aplinka
+      </button>
     </div>
   );
 };
